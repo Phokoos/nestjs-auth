8460bb3c65fe7997c4f0b77ff4f3a43439d1b944
Микола <hulkovskij@gmail.com>
2026-02-28 19:27:26 +0200

    Init

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  app.setGlobalPrefix('api');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api/docs', app, document, {
+    customSiteTitle: 'My API Docs',
+  });
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+bootstrap();
